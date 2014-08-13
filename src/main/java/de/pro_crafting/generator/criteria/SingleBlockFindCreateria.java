3b3c215a98307497c015557aedd21497b86b7bfe
Postremus <postremus1996@googlemail.com>
2014-08-13 21:42:06 +0200

    Added Simple Criteria to easify the finding of single block matches and to get the points

---
+++ src/main/java/de/pro_crafting/generator/criteria/SingleBlockFindCreateria.java
@@ -0,0 +1,43 @@
+package de.pro_crafting.generator.criteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+
+import de.pro_crafting.common.Point;
+import de.pro_crafting.generator.BlockData;
+
+public class SingleBlockFindCreateria {
+	private Criteria wraped;
+	private Material type;
+	private List<Point> matches;
+	
+	public SingleBlockFindCreateria(Material type) {
+		this.type = type;
+		this.matches = new ArrayList<Point>();
+	}
+	
+	public boolean matches(BlockData block, Point point) {
+		boolean shouldSet = block.getType() == this.type;
+		if (wraped != null && shouldSet) {
+			shouldSet = wraped.matches(block, point);
+		}
+		if (shouldSet) {
+			this.matches.add(point);
+		}
+		return true;
+	}
+
+	public void wrap(Criteria criteria) {
+		if (wraped == null) {
+			wraped = criteria;
+		} else {
+			wraped.wrap(criteria);
+		}
+	}
+	
+	public List<Point> getMatches() {
+		return this.matches;
+	}
+}
